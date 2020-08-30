5c8d4d754cb7db5ded271ae5c36ce7ab4347421b
kimnarrae <na_rea@naver.com>
2020-08-30 14:03:47 +0900

    Updates

---
+++ precache-manifest.bfa64053701fb29a69f54edb947b90ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b64f503eb364ada754dfa534003577c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f62ad5ab8aa7dfb3b9a",
+    "url": "/static/css/main.752b3588.chunk.css"
+  },
+  {
+    "revision": "4d7c806d0e258b226914",
+    "url": "/static/js/2.00a09d16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00a09d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f62ad5ab8aa7dfb3b9a",
+    "url": "/static/js/main.d2c3ea7c.chunk.js"
+  },
+  {
+    "revision": "50b67832ef5f1dc086fe",
+    "url": "/static/js/runtime-main.b486f117.js"
+  }
+]);
